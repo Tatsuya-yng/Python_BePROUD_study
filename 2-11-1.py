b859920c5e83693a5db4ab5e2d07469aeda244c5
”tanakatatsuya” <tanaka@gmail.com>
2020-06-03 21:21:45 +0900

    復習ドリル

---
+++ 2-11-1.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8
+#復習
+#6歳未満なら幼女と表示するプログラムを作る
+
+text = input( '年齢は?' )
+age = int( text )
+if age < 6 :
+    print( '幼女' )
---
+++ 2-11-2.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8
+#復習
+#問題点を探す
+
+text = input( '年齢は?' )
+age = int( text )
+if age >= 5 and age <= 65 :
+    print( '幼児と高齢者' )
