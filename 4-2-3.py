573c55c5f5c213f165f4f40387c852d51e170dc4
”tanakatatsuya” <tanaka@gmail.com>
2020-06-05 16:17:13 +0900

    関数の書き方を覚える

---
+++ 4-2-3.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8
+#関数の書き方を覚える
+#引数を受け取る関数を作る
+
+def create_mail( recv ) :
+    print( recv, '様' )
+    print('PT企画の田中です。')
+    print('今月の請求書を送ります。')
+
+create_mail('山本')
+create_mail('吉田')
