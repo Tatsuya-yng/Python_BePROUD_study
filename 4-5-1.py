a4d5922f68f869e8382ec935f1fd6024699c30ac
”tanakatatsuya” <tanaka@gmail.com>
2020-06-05 17:54:15 +0900

    辞書を使って複数のデータをまとめる

---
+++ 4-5-1.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8
+#辞書を使って複数のデータをまとめる
+#一人分のデータを辞書に入れる
+
+data = { 'name':'田中','bill':40000,'crg':True }
+print( data['name'] )
+print( data['bill'] )
---
+++ 4-5-2.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8
+#辞書を使って複数のデータをまとめる
+#複数人のデータをリストにまとめる
+
+data = [
+{'name':'田中','bill':40000,'crg':True},
+{'name':'吉田','bill':25000,'crg':False}
+]
+print(data[1]['name'])
+print(data[1]['bill'])
